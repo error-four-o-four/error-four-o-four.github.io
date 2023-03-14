13e3bee0136a5db72b964ccc7762766775e091e4
Jens B <jens.bachmayer@gmx.de>
2023-03-14 21:53:31 +0100

    fix: adjust output directory to docs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import postcssNesting from 'postcss-nested';
 import postcssCustomMedia from 'postcss-custom-media'
 
 export default defineConfig({
-
+	build: {
+		outDir: 'docs/'
+	},
 	css: {
 		postcss: {
 			plugins: [postcssCustomMedia(), postcssNesting()],
